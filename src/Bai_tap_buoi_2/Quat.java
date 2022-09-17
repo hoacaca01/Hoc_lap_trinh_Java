90c9481e4efbf827faa03ceceeb278545f39b8f3
Pham Xuan Hoa <tieucaca876@gmail.com>
2022-09-17 11:09:39 +0700

    sửa lại bài quat

--- src/Bai_tap_buoi_2/Quat.java
+++ src/Bai_tap_buoi_2/Quat.java
@@ -1,84 +1,64 @@
 package Bai_tap_buoi_2;
 
 public class Quat {
-    String thuonghieu;
+    String thuongHieu;
     String loai;
-    int gia;
-    int congsuat;
-    int capdo = 1;
-    Boolean quayngang = false;
-    int cham = 1;
-    int trungbinh = 2;
-    int nhanh = 3;
-    Boolean bat = false;
+    float gia;
+    int congSuat;
+    int capDo;
+    boolean quayNgang;
 
-    public Quat(String thuonghieu, String loai, int gia, int congsuat, int capdo, boolean bat, boolean quayngang) {
-        this.thuonghieu = thuonghieu;
+    public Quat(String thuongHieu, String loai, float gia, int congSuat) {
+        this.thuongHieu = thuongHieu;
         this.loai = loai;
         this.gia = gia;
-        this.congsuat = congsuat;
-        this.capdo = capdo;
-        this.bat = bat;
-        this.quayngang = quayngang;
+        this.congSuat = congSuat;
+        this.capDo = 0;
+        this.quayNgang = false;
     }
 
-    void hienthi() {
-        System.out.println("Thuong Hieu:" + thuonghieu + " \nLoai: " + loai + " \nGia: " + gia + " \nCong Suat: " + congsuat);
+    void chuyenSo(int so) {
+        if (so >= 0)
+            this.capDo = so;
+        else
+            System.out.println("So khong hop le");
     }
-    public int capdocham() {
-        return cham;
+
+    void chuyenTrangThaiQuay() {
+        this.quayNgang = !this.quayNgang;
     }
-    public int capdotrungbinh() {
-        return trungbinh;
-    }
-    public int capdonhanh() {
-        return nhanh;
-    }
-    public int choncapdo() {
-        return capdo;
-    }
-    void chuyencapdo(int capdo) {
-        this.capdo = capdo;
-    }
-    public boolean bat() {
-        return bat;
-    }
-    void chonbat(boolean bat) {
-        this.bat = bat;
-    }
-    public boolean quayngang(){
-        return quayngang;
-    }
-    void chuyentrangthaiquay(boolean quayngang){
-        this.quayngang = quayngang;
-    }
-    public String toString() {
-        String a = "";
-        if (this.bat()) {
-            a += ("Quat dang bat\n");
-            a += ("Quat dang o cap do: " + this.capdo + "\n");
-            if (this.quayngang()){
-                a +=("Quat dang quay ngang\n");
-            } else {
-                a +=("Quat dang dung yen");
-            }
-        } else {
-            a += ("Quat dang tat");
+
+    void bat() {
+        if (this.capDo == 0) {
+            chuyenSo(1);
         }
-        return a;
     }
+
+    void tat() {
+        chuyenSo(0);
+    }
+
+    @Override
+    public String toString() {
+        return "Quat{" +
+                "thuongHieu='" + thuongHieu + '\'' +
+                ", loai='" + loai + '\'' +
+                ", gia=" + gia +
+                ", congSuat=" + congSuat +
+                ", capDo=" + capDo +
+                ", quayNgang=" + quayNgang +
+                '}';
+    }
+
+    void hienThi() {
+        System.out.println(this);
+    }
+
     public static void main(String[] args) {
-        System.out.println("----------------------------------------");
-        Quat quat1 = new Quat("SENKO", "QUAT DUNG", 500000, 60, 3, true, true);
-        quat1.hienthi();
-        System.out.println(quat1.toString());
-        System.out.println("----------------------------------------");
-        Quat quat2 = new Quat("SENKO", "QUAT DUNG", 100000, 100, 2, false, true);
-        quat2.hienthi();
-        System.out.println(quat2.toString());
-        System.out.println("----------------------------------------");
-        Quat quat3 = new Quat("HATARI", "QUAT TREO TUONG", 300000, 80, 1, true, false);
-        quat3.hienthi();
-        System.out.println(quat3.toString());
+        Quat quat1 = new Quat("SENKO", "QUAT DUNG", 500000, 60);
+        quat1.hienThi();
+        quat1.bat();
+        quat1.chuyenTrangThaiQuay();
+        quat1.hienThi();
     }
 }
